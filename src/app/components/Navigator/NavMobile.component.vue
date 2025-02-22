b1d9079e02fc1a8155d0c0a1532585ae8cb97911
GwerhDev <gwerh.dev@gmail.com>
2025-02-22 02:51:19 -0300

    styling & adding awesome font icon

--- src/app/components/Navigator/NavMobile.component.vue
+++ src/app/components/Navigator/NavMobile.component.vue
@@ -1,14 +1,8 @@
 <style scoped lang="scss" src="./NavMobile.component.scss" />
 <script setup lang="ts">
-import { computed } from 'vue';
-import { useStore } from '../../../middlewares/store';
 import { openMenu, closeMenu, openAccountMenu, openAppMenu } from '../../../helpers/menu';
 import Logo from '../Logo/Logo.component.vue';
 import MenuMobile from '../Menu/MenuMobile.component.vue';
-
-const store: any = useStore();
-const currentUser: any = computed(() => store.currentUser);
-const logged: any = computed(() => currentUser.value.logged);
 
 </script>
 
@@ -16,11 +10,11 @@ const logged: any = computed(() => currentUser.value.logged);
   <ul class="nav-menu-ul">
     <div class="d-flex align-cent logo-container">
       <div class="d-flex align-cent logo-cont">
-        <div v-if="logged" class="burger-menu-container">
+        <div class="burger-menu-container">
           <img src="../../../assets/svg/icon-burger-menu.svg" @click="openMenu()" alt="">
         </div>
         <div class="close-menu-container" alt="">
-          <img v-if="logged" src="../../../assets/svg/close-icon.svg" @click="closeMenu()" alt="">
+          <img src="../../../assets/svg/close-icon.svg" @click="closeMenu()" alt="">
         </div>
         <Logo :width="'40'"  @click="closeMenu()" />
       </div>
