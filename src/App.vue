7a866ad0b63a7ab2070445b5160e39f6e9d4d2fc
GwerhDev <gwerh.dev@gmail.com>
2025-02-22 02:57:11 -0300

    styling & adding awesome font icon

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { useStore } from './middlewares/store/index';
 import NavBar from './app/components/Navigator/NavBar.component.vue';
-
-const store: any = useStore();
-const token: any = localStorage.getItem('userToken');
-const currentUser: any = computed(() => store.currentUser);
-const logged: any = computed(() => currentUser.value.logged);
-token && store.handleUserData(token);
 
 </script>
 
