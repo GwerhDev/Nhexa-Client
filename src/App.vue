a55e1a9d5daf65546cb5f18bc5614950211b001a
GwerhDev <gwerh.dev@gmail.com>
2025-03-28 01:07:02 -0300

    rectifying loging system

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import NavBar from './app/components/Navigator/NavBar.component.vue';
 import { onMounted, ref } from 'vue';
+import { getUserToken } from './helpers';
 import { useStore } from './middlewares/store/index';
 
 const store: any = useStore();
-const authFrame = ref<HTMLIFrameElement | null>(null); // Referencia al iframe
 
 onMounted(async () => {
   const token: any = await getUserToken();
