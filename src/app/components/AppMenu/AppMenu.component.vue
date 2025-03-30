15117a3bf0e6b5f7582d1a1d90fe31d401ddeff5
GwerhDev <gwerh.dev@gmail.com>
2025-03-30 12:27:36 -0300

    adding loader component

--- src/app/components/AppMenu/AppMenu.component.vue
+++ src/app/components/AppMenu/AppMenu.component.vue
@@ -3,6 +3,7 @@
 import { onMounted, computed, Ref } from 'vue';
 import { useStore } from '../../../middlewares/store';
 import { CanvasMenuFunction, closeAccountMenu, closeMenu } from '../../../helpers/menu';
+import Loader from '../Loader/Loader.component.vue';
 
 const store = useStore();
 const appList: Ref<any[]> = computed(() => store.appList);
@@ -32,8 +33,8 @@ onMounted(() => {
         {{ item.label }}
       </a>
     </li>
-    <li v-else>
-      <div class="loader"></div>
+    <li class="loader-container" v-else>
+      <Loader />
     </li>
   </ul>
 </template>
