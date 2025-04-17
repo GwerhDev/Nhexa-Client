b61efd663e66c3210bc11a50d1c8ef3b74fdf9ae
gerardo <gerardo@recemed.com>
2025-04-17 09:24:20 -0400

    adding credentials to app-list

--- src/middlewares/services/index.ts
+++ src/middlewares/services/index.ts
@@ -3,7 +3,7 @@ import { API_URL } from "../misc/const";
 import { error } from "../misc/errors";
 
 export const getAppList: any = async () => {
-  const response: any = await axios.get(API_URL + "/app-list")
+  const response: any = await axios.get(API_URL + "/app-list", { withCredentials: true })
     .then(response => response.data)
     .catch(() => { return { error: error.api.loadItemById } });
   return response;
