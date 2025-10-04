c28a61545cbbf209d1859d6108a79fadc2154623
GwerhDev <gwerh.dev@gmail.com>
2025-10-04 07:00:46 -0300

    migrating api with streamby

--- src/middlewares/services/index.ts
+++ src/middlewares/services/index.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { API_URL } from "../misc/const";
+import { API_URL, STREAMBY_URL } from "../misc/const";
 import { error } from "../misc/errors";
 
 export const getAppList: any = async () => {
-  const response: any = await axios.get(API_URL + "/app-list", { withCredentials: true })
+  const response: any = await axios.get(STREAMBY_URL + "/app-list", { withCredentials: true })
     .then(response => response.data)
     .catch(() => { return { error: error.api.loadItemById } });
   return response;
