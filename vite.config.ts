790c65ff5786623d9f95238899a21f37df010afd
GwerhDev <gwerh.dev@gmail.com>
2026-06-11 19:20:08 -0400

    feat: add image compressor for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+function imagesToWebp(): Plugin {
+  return {
+    name: 'images-to-webp',
+    apply: 'build',
+    async load(id: string) {
+      if (!/\.(png|jpe?g|jpg)$/i.test(id) || id.includes('\x00')) return null;
+      const { default: sharp } = await import('sharp');
+      const webpBuffer = await sharp(id).webp({ quality: 82 }).toBuffer();
+      const ref = this.emitFile({
+        type: 'asset',
+        name: path.basename(id, path.extname(id)) + '.webp',
+        source: webpBuffer,
+      });
+      return `export default import.meta.ROLLUP_FILE_URL_${ref}`;
+    }
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), imagesToWebp()],
   assetsInclude: ['**/*.glb'],
 })
