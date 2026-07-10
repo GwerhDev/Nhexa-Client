e7a3c1dfd843dba92a307391a010c8697cc69e05
GwerhDev <gwerh.dev@gmail.com>
2026-07-10 11:04:41 -0400

    Revert "feat: white-to-colorful fluid trail + text above effect"
    This reverts commit e7b0946e2677c15761c01aef986ebe9f37c1b885.

---
+++ src/webgl-fluid.d.ts
@@ -0,0 +1,7 @@
+declare module 'webgl-fluid' {
+  type WebGLFluidConfig = Record<string, unknown>;
+  export default function WebGLFluid(
+    canvas: HTMLCanvasElement,
+    config?: WebGLFluidConfig,
+  ): void;
+}
